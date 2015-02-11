5cf57914d0022b696d8e6295c63a57920bcb8416
Ben Kujala <ben.kujala@gmail.com>
2015-02-11 10:17:23 -0800

    Initial Dialog Design
    Added the form elements for the application dialog.

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ConservWBExtract
 {
@@ -14,6 +16,60 @@ namespace ConservWBExtract
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = false;
+            if (textBox1.Text != "")
+            {
+                folderDlg.SelectedPath = textBox1.Text;
+            }
+            // Show the file dialog
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK) {
+                textBox1.Text = folderDlg.SelectedPath;
+            }
+        }
+
+        private void btnFileName_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog outputDlg = new SaveFileDialog();
+            outputDlg.DefaultExt = ".csv";
+            outputDlg.Filter = "Comma Separated Variable (*.csv)|*.csv";
+            outputDlg.SupportMultiDottedExtensions = false;
+            outputDlg.AddExtension = false;
+            if (textBox2.Text != "")
+            {
+                outputDlg.FileName = textBox2.Text;
+            }
+            DialogResult result = outputDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = outputDlg.FileName;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            // If no folder is selected then throw an error
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("No Folder Selected", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // First search the files in the parent directory
+            foreach (string strFile in Directory.GetFiles(textBox1.Text,"*.*", SearchOption.AllDirectories))
+            {
+                Match match = Regex.Match(strFile, @".*\.xls.*", RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    // You've found an Excel file...
+                    System.Diagnostics.Debug.WriteLine(strFile);
+                }
+            }
+            this.Visible = true;
         }
     }
 }
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ConservWBExtract.Properties
-{
-
-
+namespace ConservWBExtract.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LastFolder {
+            get {
+                return ((string)(this["LastFolder"]));
+            }
+            set {
+                this["LastFolder"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LastFile {
+            get {
+                return ((string)(this["LastFile"]));
+            }
+            set {
+                this["LastFile"] = value;
             }
         }
     }
